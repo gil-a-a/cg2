2d332feb7c5a723b39c77f3808e4272676dcebf9
gil-a-a <98371042+gil-a-a@users.noreply.github.com>
2023-05-05 18:59:23 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "screen.h"
+#include "point2D.h"
+#include "color.h"
+#include "line.h"
+#include "picture.h"
+#include "circle.h"
+#include "polygon.h"
+
+int main(int argc, char *argv[])
+{
+    /*Screen screen("Aula 3", BLACK, 700, 700);
+
+    Point2D p3(350, 450);
+
+    Picture p;
+    
+    // Telhado
+    Line l1(Point2D(350, 50), Point2D(50, 250), RED);
+    Line l2(Point2D(50, 250), Point2D(650, 250), RED);
+    Line l3(Point2D(650, 250), Point2D(350, 50), RED);
+        
+    // Parede
+    Line l4(Point2D(50, 250), Point2D(50, 650), GREEN);
+    Line l5(Point2D(50, 650), Point2D(650, 650), GREEN);
+    Line l6(Point2D(650, 650), Point2D(650, 250), GREEN);
+        
+    Circle circ(p3, 150, BLUE);
+        
+    // Insere primitivas na lista
+    p.add(&l1);
+    p.add(&l2);
+    p.add(&l3);
+    p.add(&l4);
+    p.add(&l5);
+    p.add(&l6);
+    p.add(&circ);
+        
+    screen.run(p);*/
+    
+    // Aula - Preenchimento de Polígonos
+    Screen screen2("Aula 4", BLACK, 700, 700);
+
+	//Exemplo 1
+//	/*
+	Polygon pol(true, RED, true, WHITE);
+	Point2D v1(20,30,GREEN);
+	Point2D v2(70,10,GREEN);
+	Point2D v3(130,50,GREEN);
+	Point2D v4(130,110,GREEN);
+	Point2D v5(70,70,GREEN);
+	Point2D v6(20,90,GREEN);
+	
+	pol.addVertex(&v1);
+	pol.addVertex(&v2);
+	pol.addVertex(&v3);
+	pol.addVertex(&v4);
+	pol.addVertex(&v5);
+	pol.addVertex(&v6);
+//	*/
+	
+	//Exemplo 2
+	/*
+	Polygon pol2(true, RED, true, WHITE);
+	Point2D v1pol2(20,30,GREEN);
+	Point2D v2pol2(35, 30,GREEN);
+	Point2D v3pol2(35, 60,GREEN);	
+	Point2D v4pol2(70, 30,GREEN);
+	Point2D v5pol2(70, 60,GREEN);
+	Point2D v6pol2(130, 30, GREEN);
+	Point2D v7pol2(130,90,GREEN);
+	Point2D v8pol2(20,90,GREEN);
+	
+	pol2.addVertex(&v1pol2);
+	pol2.addVertex(&v2pol2);
+	pol2.addVertex(&v3pol2);
+	pol2.addVertex(&v4pol2);
+	pol2.addVertex(&v5pol2);
+	pol2.addVertex(&v6pol2);
+	pol2.addVertex(&v7pol2);
+	pol2.addVertex(&v8pol2);
+	*/
+	
+	//Exemplo 3
+	/*
+	Polygon pol3(true, RED, true, WHITE);
+	Point2D v1pol3(20, 50, GREEN);
+	Point2D v2pol3(45, 50, GREEN);
+	Point2D v3pol3(70, 20, GREEN);
+	Point2D v4pol3(95, 50, GREEN);
+	Point2D v5pol3(120, 50, GREEN);
+	Point2D v6pol3(95, 70, GREEN);
+	Point2D v7pol3(105, 120, GREEN);
+	Point2D v8pol3(70, 90, GREEN);
+	Point2D v9pol3(45, 120, GREEN);
+	Point2D v10pol3(55, 70, GREEN);
+	
+	pol3.addVertex(&v1pol3);
+	pol3.addVertex(&v2pol3);
+	pol3.addVertex(&v3pol3);
+	pol3.addVertex(&v4pol3);
+	pol3.addVertex(&v5pol3);
+	pol3.addVertex(&v6pol3);
+	pol3.addVertex(&v7pol3);
+	pol3.addVertex(&v8pol3);
+	pol3.addVertex(&v9pol3);
+	pol3.addVertex(&v10pol3);
+	*/
+	
+	Picture p2;
+	p2.add(&pol);
+//	p2.add(&pol2);
+//	p2.add(&pol3);
+	
+	screen2.run(p2);
+
+    return 0;
+}
---
+++ polygon.cpp
@@ -0,0 +1,167 @@
+#include <iterator>
+#include <iostream>
+#include <cmath>
+#include "polygon.h"
+#include "line.h"
+#include "edgeTable.h"
+#include "activeEdgeTable.h"
+
+Polygon::Polygon(bool showE, Color edgeC, bool fill, Color fillC):Primitive("Polygon"), showEdges(showE), edgeColor(edgeC), isFilled(fill), fillColor(fillC) {
+	
+}
+
+void Polygon::draw(Screen &screen) const {
+	if (listOfPoints.size() < 3) {
+		std::cout << "Nao forma polígono. Menos de 3 vértices.\n";
+        return;
+	}
+            
+    // Desenha arestas se desejar
+    if (isFilled)
+        scanline(screen);
+        
+    if (showEdges) {
+    	list<Point2D *>::const_iterator it;
+    	it = listOfPoints.begin();
+    	for (int i = 0; i < listOfPoints.size() - 1; i++, ++it) {
+    		Point2D *pI = *it;
+    		Point2D *pF = *(next(it));
+    		
+    		Line line = Line(*pI, *pF, edgeColor);
+    		line.draw(screen);
+		}
+    	
+    	it = prev(listOfPoints.end());
+    	Point2D *pI = *it;
+    	std::cout << "I(" << pI->getX() << ", " << pI->getY() << ")\n";
+    	it = listOfPoints.begin();
+    	Point2D *pF = *it;
+    	std::cout << "I(" << pF->getX() << ", " << pF->getY() << ")\n";
+    	Line line = Line(*pI, *pF, edgeColor);
+        line.draw(screen);
+        
+        for (it = listOfPoints.begin(); it != listOfPoints.end(); ++it)
+        	(*it)->draw(screen);
+	}
+}
+
+void Polygon::addVertex(Point2D *p) {
+	listOfPoints.push_back(p);
+}
+
+void Polygon::scanline (Screen & screen) const {
+	EdgeTable ET(listOfPoints);
+	
+	ActiveEdgeTable AET;
+	
+	int y = ET.getYMin();
+	
+    int bits[2][2] = {{1,1},{0,0}};
+    int i, j;
+    // Laço principal
+	while (y <= ET.getYMax()) {
+        //# Move a lista y na ET para AET (ymin = y), mantendo a AET ordenada em x
+        AET.extend(ET.getList(y));
+        ET.clearList(y);
+        AET.sort();
+              
+        // Desenhe os pixels do bloco na linha de varredura y, 
+        // usando os pares de coordenadas x da AET (cada dois nós definem um bloco)
+		double xI, xF;
+		i = 0;
+        j = 0;
+        while (AET.getOneBlockLimits(&xI, &xF)) {
+        	
+			for (int x = round(xI); x <= floor(xF); x++) {
+				if (i > 1)
+					i = 0;
+				if (j > 1){
+					j = 0;
+					i++;
+				}
+				
+				if(bits[i][j] == 1)
+					screen.setPixel(x, y, fillColor);
+				
+				j++;
+			}
+        }
+        
+		std::cout << "Linha y = " << y <<": ";
+		sequenciaBit(AET);
+    	
+        //Atualiza o valor de y para a próxima linha de varredura
+        y = y + 1;
+        
+        // Remova as arestas que possuem ymax = y da AET
+        AET.removeEdgesByYMax(y);
+            
+        // Para cada aresta na AET, atualize x = x + 1/m
+        AET.updateXValues();
+    }
+    
+    /*
+    while (y <= ET.getYMax()) {
+        //# Move a lista y na ET para AET (ymin = y), mantendo a AET ordenada em x
+        AET.extend(ET.getList(y));
+        ET.clearList(y);
+        AET.sort();
+              
+        // Desenhe os pixels do bloco na linha de varredura y, 
+        // usando os pares de coordenadas x da AET (cada dois nós definem um bloco)
+		double xI, xF;
+        while (AET.getOneBlockLimits(&xI, &xF)) {
+        	for (int x = round(xI); x <= floor(xF); x++)	
+        		screen.setPixel(x, y, fillColor);
+        }
+        
+		std::cout << "Linha y = " << y <<": ";
+		sequenciaBit(AET);
+    	
+        //Atualiza o valor de y para a próxima linha de varredura
+        y = y + 1;
+        
+        // Remova as arestas que possuem ymax = y da AET
+        AET.removeEdgesByYMax(y);
+            
+        // Para cada aresta na AET, atualize x = x + 1/m
+        AET.updateXValues();
+    }
+    */
+}
+
+/*
+1. Estenda o código criando um método que recebe uma tabela de arestas ativas (AET) de uma determinada linha de varredura e retornando 
+uma lista com as paridades em cada aresta. Por exemplo, para o polígono do exemplo de aula, e a AET correspondente à linha de varredura y = 8, 
+nós teremos 4 arestas sendo interceptadas, sendo que o algoritmo de paridade retornaria a sequência 1 0 1 0 para o bit de paridade em cada aresta. 
+Faça ao menos 3 exemplos de linha de varredura para mostrar o funcionamento.
+*/
+//Pra achar a sequência que o enunciado fala, precisa percorrer a AET e adicionar 1 em iteração par e 0 em ímpar
+void Polygon::sequenciaBit(ActiveEdgeTable AET) const {
+	int size = AET.getSize();
+	for (int i = 0; i < size; i++){
+		if (i%2 == 0)
+			std::cout << "1";
+		else 
+			std::cout << "0";
+	}
+	std::cout << "\n";
+}
+
+/*
+2. Modifique a implementação do algoritmo Scanline para gerar áreas preenchidas com determinado padrão de preenchimento (hachuras). 
+A ideia é fazer uma implementação que aceite qualquer padrão passado como parâmetro. 
+Uma ideia consiste em usar uma matriz para representar um padrão, composto de valores 0 e 1 que definem a hachura a ser desenhada. 
+Faça 3 exemplos usando três padrões diferentes. DICA: Lembre do preenchimento de circunferências.
+*/
+
+
+
+
+
+
+
+
+
+
+
